7a2ce1c6b167023355db75cac77ca5060955fdf6
CORP\icasasdu <irina.casas-duarte@capgemini.com>
2024-05-30 15:20:47 +0200

    añadidos métodos en IntegrationJPATest

--- README.md
+++ README.md
@@ -53,8 +53,8 @@
 
 
 2. Para cada uno de los tests tenemos que **configurar el entorno**, ya que la propiedad ENVIRONMENT no existe lo haremos de la siguiente manera:
-          1. *Edit Configurations -> Environment variables -> ENVIRONMENT=DEV(o PROD)*
-          2. "Build and run" donde aparece la versión de java, en el campo VM options(lo sabremos poniendo el ratón encima) escribimos lo siguiente *-ea -DENV=dev* o  *-ea -DENV=prod*
+- *Edit Configurations -> Environment variables -> ENVIRONMENT=DEV(o PROD)*
+- "Build and run" donde aparece la versión de java, en el campo VM options(lo sabremos poniendo el ratón encima) escribimos lo siguiente *-ea -DENV=dev* o  *-ea -DENV=prod*
 
 
 
--- src/test/java/JUnitMockitoProject/JPATest/IntegrationJPATest.java
+++ src/test/java/JUnitMockitoProject/JPATest/IntegrationJPATest.java
@@ -1,7 +1,44 @@
 package JUnitMockitoProject.JPATest;
 
+import JUnitMockitoProject.Entities.AccountProject;
+import JUnitMockitoProject.Respositories.AccountProjectRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class IntegrationJPATest {
+    @Autowired
+    AccountProjectRepository accountProjectRepository;
+
+    @Test
+    void findById_test() {
+        Optional<AccountProject> account = accountProjectRepository.findById(1L);
+        assertTrue(account.isPresent());
+        assertEquals("Irina", account.orElseThrow().getPerson());
+    }
+
+    @Test
+    void findByPerson_test() {
+        Optional<AccountProject> account = accountProjectRepository.findByPerson("Irina");
+        assertThrows(NoSuchElementException.class, () -> {
+            account.orElseThrow();
+        });
+        assertTrue(account.isPresent());
+        assertEquals("Irina", account.orElseThrow().getPerson());
+        assertEquals("1000", account.orElseThrow().getBalance());
+    }
+
+    @Test
+    void findAll_test() {
+        List<AccountProject> accountsProject = accountProjectRepository.findAll();
+        assertFalse(accountsProject.isEmpty());
+        assertEquals(2, accountsProject.size());
+    }
 }
